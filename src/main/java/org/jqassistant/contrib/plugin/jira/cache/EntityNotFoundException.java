7bebb13c2b69afdbfc28815049ac5ee9a615af59
Stephan Pirnbaum <stephan.pirnbaum@googlemail.com>
2023-05-25 10:23:51 +0200

    Initial rework for new release

--- src/main/java/org/jqassistant/contrib/plugin/jira/cache/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.jqassistant.contrib.plugin.jira.cache;
-
-public class EntityNotFoundException extends RuntimeException {
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/org/jqassistant/contrib/plugin/jira/model/basic/Jira.java
+++
@@ -1,8 +0,0 @@
-package org.jqassistant.contrib.plugin.jira.model.basic;
-
-import com.buschmais.jqassistant.core.store.api.model.Descriptor;
-import com.buschmais.xo.neo4j.api.annotation.Label;
-
-@Label("Jira")
-public interface Jira extends Descriptor {
-}
